a020144b3097077f24d361f6a6eb1fd8ff5975da
YutaKakiki <uta31501224@gmail.com>
2025-05-04 02:43:26 +0900

    ♻️ Fix structre of protocol-handlers

--- shared/protocol/handlers.go
+++ shared/protocol/handlers.go
@@ -2,6 +2,12 @@ package protocol
 
 import "github.com/kakkky/mcp-sdk-go/shared/schema"
 
+type handlers struct {
+	requestHandlers      map[string]requestHandler
+	notificationHandlers map[string]notificationHandler
+	responseHandlers     map[int]responseHandler
+}
+
 type requestHandler = func(request schema.JsonRpcRequest)
 
 type notificationHandler = func(notification schema.JsonRpcNotification)
--- shared/protocol/protocol.go
+++ shared/protocol/protocol.go
@@ -5,24 +5,36 @@ import (
 )
 
 type Protocol struct {
-	transport            Transport
-	requestHandlers      map[string]requestHandler
-	notificationHandlers map[string]notificationHandler
-	responseHandlers     map[int]responseHandler
+	transport Transport
+	handlers  handlers
+}
+
+func NewProtocol() *Protocol {
+	return &Protocol{
+		handlers: handlers{
+			requestHandlers:      make(map[string]requestHandler),
+			notificationHandlers: make(map[string]notificationHandler),
+			responseHandlers:     make(map[int]responseHandler),
+		},
+	}
 }
 
 func (p *Protocol) SetRequestHandler(requestSchema schema.Request, handler func(request schema.JsonRpcRequest)) {
 	method := requestSchema.Method
 	// TODO: ここで、指定されたmethodをすでに登録していないか確認
-	p.requestHandlers[method] = handler
+	p.handlers.requestHandlers[method] = handler
 }
 
 func (p *Protocol) SetNotificationHandler(notificationSchema schema.Notification, handler func(notification schema.JsonRpcNotification)) {
 	method := notificationSchema.Method
-	p.notificationHandlers[method] = handler
+	p.handlers.notificationHandlers[method] = handler
 }
 
 // リクエスト送信の際に、対応するレスポンスハンドラを登録する
 func (p *Protocol) SetResponseHandler(messageId int, handler func(response schema.JsonRpcResponse)) {
-	p.responseHandlers[messageId] = handler
+	p.handlers.responseHandlers[messageId] = handler
+}
+
+func (p *Protocol) Connect(transport Transport) {
+
 }
