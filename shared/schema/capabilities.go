bb66e94f0b1a097be3ab46a75e484907b9d3e988
YutaKakiki <uta31501224@gmail.com>
2025-05-10 04:24:43 +0900

    🏷️ Define type of capabilities

---
+++ shared/schema/capabilities.go
@@ -0,0 +1,37 @@
+package schema
+
+// Server
+type ServerCapabilities struct {
+	Experimental any `json:"experimental,omitempty"`
+	*Logging     `json:"logging"`
+	*Prompts     `json:"prompts,omitempty"`
+	*Resources   `json:"resources,omitempty"`
+	*Tools       `json:"tools,omitempty"`
+}
+
+type Logging struct{}
+
+type Prompts struct {
+	ListChanged bool `json:"listChanged,omitempty"`
+}
+
+type Resources struct {
+	Subscribe   bool `json:"subscribe,omitempty"`
+	ListChanged bool `json:"listChanged,omitempty"`
+}
+type Tools struct {
+	ListChanged bool `json:"listChanged,omitempty"`
+}
+
+// Client
+type ClientCapabilities struct {
+	Experimental any `json:"experimental,omitempty"`
+	*Sampling    `json:"sampling"`
+	*Roots       `json:"roots,omitempty"`
+}
+
+type Sampling struct{}
+
+type Roots struct {
+	ListChanged bool `json:"listChanged,omitempty"`
+}
