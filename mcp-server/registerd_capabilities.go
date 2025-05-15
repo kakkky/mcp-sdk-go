b4b05278f6f7c45a190bf71abf7046737fbad1ce
YutaKakiki <uta31501224@gmail.com>
2025-05-15 23:59:48 +0900

    🎨 Restructured the file organization

--- mcp-server/registerd_capabilities.go
+++ mcp-server/registerd_capabilities.go
@@ -4,7 +4,6 @@ import (
 	"net/url"
 
 	"github.com/kakkky/mcp-sdk-go/shared/schema"
-	utilities "github.com/kakkky/mcp-sdk-go/shared/utilities/uri-template"
 )
 
 type RegisteredResource struct {
@@ -44,41 +43,3 @@ type RegisteredResourceTemplate struct {
 }
 
 type ReadResourceTemplateCallback[T schema.ResourceContentSchema] func(url url.URL, variables map[string]any) schema.ReadResourceResultSchema[T]
-
-type ResourceTemplate struct {
-	uriTemp   *utilities.UriTemplate
-	callBacks *struct {
-		list     *ListResourcesCallback
-		complete map[string]*CompleteResourceCallback
-	}
-}
-type ListResourcesCallback func() schema.ListResourcesResultSchema
-type CompleteResourceCallback func() []string
-
-func NewResourceTemplate(uriTemplate string) (*ResourceTemplate, error) {
-	uriTemp, err := utilities.NewUriTemplate(uriTemplate)
-	if err != nil {
-		return nil, err
-	}
-	return &ResourceTemplate{
-		uriTemp: uriTemp,
-	}, nil
-}
-
-func (r *ResourceTemplate) uriTemplate() *utilities.UriTemplate {
-	return r.uriTemp
-}
-
-func (r *ResourceTemplate) ListCallback() *ListResourcesCallback {
-	if r.callBacks == nil {
-		return nil
-	}
-	return r.callBacks.list
-}
-
-func (r *ResourceTemplate) CompleteCallBack(variable string) *CompleteResourceCallback {
-	if r.callBacks == nil {
-		return nil
-	}
-	return r.callBacks.complete[variable]
-}
---
+++ mcp-server/resource_template.go
@@ -0,0 +1,44 @@
+package mcpserver
+
+import (
+	"github.com/kakkky/mcp-sdk-go/shared/schema"
+	utilities "github.com/kakkky/mcp-sdk-go/shared/utilities/uri-template"
+)
+
+type ResourceTemplate struct {
+	uriTemp   *utilities.UriTemplate
+	callBacks *struct {
+		list     *ListResourcesCallback
+		complete map[string]*CompleteResourceCallback
+	}
+}
+type ListResourcesCallback func() schema.ListResourcesResultSchema
+type CompleteResourceCallback func() []string
+
+func NewResourceTemplate(uriTemplate string) (*ResourceTemplate, error) {
+	uriTemp, err := utilities.NewUriTemplate(uriTemplate)
+	if err != nil {
+		return nil, err
+	}
+	return &ResourceTemplate{
+		uriTemp: uriTemp,
+	}, nil
+}
+
+func (r *ResourceTemplate) uriTemplate() *utilities.UriTemplate {
+	return r.uriTemp
+}
+
+func (r *ResourceTemplate) ListCallback() *ListResourcesCallback {
+	if r.callBacks == nil {
+		return nil
+	}
+	return r.callBacks.list
+}
+
+func (r *ResourceTemplate) CompleteCallBack(variable string) *CompleteResourceCallback {
+	if r.callBacks == nil {
+		return nil
+	}
+	return r.callBacks.complete[variable]
+}
