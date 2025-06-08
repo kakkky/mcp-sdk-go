9cb1e06cb5fbbaa8faca68c04cec366ac0e77111
YutaKakiki <uta31501224@gmail.com>
2025-06-08 15:19:50 +0900

    ✨ Add an implementation that sets a resource template in the example.

--- examples/server/with-stdio/main.go
+++ examples/server/with-stdio/main.go
@@ -20,6 +20,7 @@ func main() {
 				Resources: &schema.Resources{
 					ListChanged: true,
 				},
+				Completion: &schema.Completion{},
 			},
 		})
 	// Resourceを登録
@@ -41,8 +42,66 @@ func main() {
 			}}, nil
 		},
 		nil)
+
+	// Resource Templateを登録
+	template, err := mcpserver.NewResourceTemplate("file://sample/{variable}", &mcpserver.ResourceTemplateCallbacks{
+		List: func() schema.ListResourcesResultSchema {
+			return schema.ListResourcesResultSchema{
+				Resources: []schema.ResourceSchema{
+					{
+						Name: "example-template",
+						Uri:  "file://sample/example-value",
+					},
+					{
+						Name: "another-template",
+						Uri:  "file://sample/another-value",
+					},
+				},
+			}
+		},
+		Complete: map[string]mcpserver.CompleteResourceCallback{
+			"variable": func(value string) []string {
+				// ここでは単純に固定の値を返すが、実際には何らかのロジックで候補を生成する
+				return []string{"example-value", "another-value"}
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	mcpServer.Resource(
+		"example-template",
+		"",
+		template,
+		&schema.ResourceMetadata{
+			Description: "This is an example resource template",
+			MimeType:    "text/plain",
+		},
+		nil,
+		func(url url.URL, variables map[string]any) (schema.ReadResourceResultSchema, error) {
+			switch variables["variable"] {
+			case "example-value":
+				return schema.ReadResourceResultSchema{Contents: []schema.ResourceContentSchema{
+					&schema.TextResourceContentsSchema{
+						UriData:      "file://sample/example-value",
+						MimeTypeData: "text/plain",
+						ContentData:  "This is the content of the example resource template",
+					},
+				}}, nil
+			case "another-value":
+				return schema.ReadResourceResultSchema{Contents: []schema.ResourceContentSchema{
+					&schema.TextResourceContentsSchema{
+						UriData:      "file://sample/another-value",
+						MimeTypeData: "text/plain",
+						ContentData:  "This is the content of another example resource template",
+					},
+				}}, nil
+			}
+			return schema.ReadResourceResultSchema{}, nil
+		},
+	)
 	transport := transport.NewStdioServerTransport()
-	err := mcpServer.Connect(transport)
+	err = mcpServer.Connect(transport)
 	if err != nil {
 		panic(err)
 	}
